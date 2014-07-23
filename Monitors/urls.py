23c3a898791242fd47fc35c59045dad968906bc3
Mickaël <mickael@securem.eu>
2014-07-24 00:01:27 +0200

    Customized Monitor details page, with a table of Heartbeats

--- Monitors/urls.py
+++ Monitors/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, url, include
 from models import Host, Monitor
 from bsct.urls import URLGenerator
+from NetworkTests.views import PerfMonitor
 
 bsct_patterns_Host = URLGenerator( Host ).get_urlpatterns( paginate_by = 10 )
 bsct_patterns_Mon = URLGenerator( Monitor ).get_urlpatterns( paginate_by = 10 )
 
 urlpatterns = patterns('Monitors.views',
                        
+                url( 'monitor/(?P<pk>\d+)$',
+                   PerfMonitor.as_view(),
+                   name = 'monitor_detail'),
                 url( '', include( bsct_patterns_Host ) ),
                 url( '', include( bsct_patterns_Mon ) ),
               )
--- NetworkTests/views.py
+++ NetworkTests/views.py
@@ -1,3 +1,25 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import get_object_or_404
+from NetworkTests.models import HeartBeat
+from Monitors.models import Host,Monitor
+from datetime import datetime
 
-# Create your views here.
+# Showing HB
+class PerfMonitor( DetailView ):
+    model = Monitor
+    template_name = 'dataviz/list_heartbeats.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(PerfMonitor, self).get_context_data(**kwargs)
+        
+        hbs = HeartBeat.objects.filter(monitor = context['object'])
+        
+        context['object'].uptime_pc = round(100.0 * len(hbs.filter(is_up=True)) / len(hbs))
+        context['object'].meandelay = 0 # TODO
+        
+        context['object'].heartbeats = hbs
+        
+        return context
