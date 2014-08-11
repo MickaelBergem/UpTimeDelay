ee23b50abd9a3704cfc363f3538dc9e2ed6175de
Mickaël Bergem <mickaelb@theodo.fr>
2014-08-11 13:58:32 +0200

    WIP

--- NetworkTests/views.py
+++ NetworkTests/views.py
@@ -6,19 +6,24 @@ from django.shortcuts import get_object_or_404
 from NetworkTests.models import HeartBeat
 from Monitors.models import Host,Monitor
 
+
 # Showing HB
-class PerfMonitor( DetailView ):
+class PerfMonitor(DetailView):
     model = Monitor
     template_name = 'dataviz/list_heartbeats.html'
 
     def get_context_data(self, **kwargs):
         context = super(PerfMonitor, self).get_context_data(**kwargs)
-        
+
         hbs = HeartBeat.objects.filter(monitor = context['object'])
-        
-        context['object'].uptime_pc = round(100.0 * len(hbs.filter(is_up=True)) / len(hbs))
-        context['object'].meandelay = round(sum(hb.delay for hb in hbs if hb.is_up) / len(hbs))
-        
+
+        if len(hbs) > 0:
+            context['object'].uptime_pc = round(100.0 * len(hbs.filter(is_up=True)) / len(hbs))
+            context['object'].meandelay = round(sum(hb.delay for hb in hbs if hb.is_up) / len(hbs))
+        else:
+            context['object'].uptime_pc = "NA"
+            context['object'].meandelay = "NA"
+
         context['object'].heartbeats = hbs
-        
+
         return context
